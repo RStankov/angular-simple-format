50a37cb36e27f0a6af23f7febee310827242ee8d
Radoslav Stankov <rstankov@gmail.com>
2014-05-01 13:07:22 +0300

    First test

--- tests/angular_simple_format_test.js
+++ tests/angular_simple_format_test.js
@@ -1,5 +1,20 @@
+"use strict";
+
+function loadFilter(module, name) {
+  name || (name = module);
+
+  var filter;
+  angular.injector(['ng', module], false).invoke(function($filter) {
+    filter = $filter(name);
+  });
+
+  return filter;
+}
+
 describe('Angular Simple Format', function() {
-  it ("do someting", function () {
-    expect(isLoaded).to.eq(true);
+  var simpleFormat = loadFilter('simpleFormat')
+
+  it ("return string as it is if not formatting is needed", function () {
+    expect(simpleFormat('string')).to.eq('string');
   });
 });
