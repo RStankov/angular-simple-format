8d04fd3fc88dff086a291bd51031975fe63b68f5
Radoslav Stankov <rstankov@gmail.com>
2014-05-02 15:16:33 +0300

    append linebreaks after newlines

--- tests/angular_simple_format_test.js
+++ tests/angular_simple_format_test.js
@@ -14,14 +14,19 @@ function loadFilter(module, name) {
 describe('Angular Simple Format', function() {
   var simpleFormat = loadFilter('simpleFormat')
 
-  it ("return string as it is if not formatting is needed", function () {
+  it("doesn't format text if not needed", function() {
     expect(simpleFormat('string')).to.eq('string');
   });
 
-  it ("return string as it even other data types are provided", function () {
+  it("convert input data to string", function() {
     expect(simpleFormat(null)).to.eq('');
     expect(simpleFormat(false)).to.eq('');
     expect(simpleFormat(undefined)).to.eq('');
     expect(simpleFormat(1)).to.eq('1');
+  });
+
+  it("appends linebreaks after newlines", function() {
+    expect(simpleFormat("line 1\nline 2")).to.eq('line 1\n<br>line 2');
+    expect(simpleFormat("line 1\nline 2\nline 3")).to.eq('line 1\n<br>line 2\nline 3');
   });
 });
